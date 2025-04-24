ab6c66f29331ca5838d76728e8335260be3a0e33
kanna-7 <vithanalapraveen069@gmail.com>
2025-04-24 15:54:45 +0530

    put

--- backend/controllers/storeRouter.js
+++ backend/controllers/storeRouter.js
@@ -14,7 +14,7 @@ storeRouter.get('/products', (req, res) => {
     res.status(200).send(products);
   });
 
-  storeRouter.post('/order', async (req, res) => {
+storeRouter.post('/order', async (req, res) => {
     try {
       const { username, items, shippingAddress } = req.body;
       const newOrder = new store({ username, items, shippingAddress });
@@ -23,6 +23,28 @@ storeRouter.get('/products', (req, res) => {
     } catch (error) {
       console.log(error);
       res.status(500).json({ message: 'Error placing order', error });
+    }
+  });
+storeRouter.put('/updatestore/:id', async (req, res) => {
+    try {
+      const {id} = req.params;
+      if(!id){
+        return res.status(400).send({message:"Please provide id"});
+      }
+      const {username, items, shippingAddress } = req.body;
+      const updatedstore = await store.findOneAndUpdate({_id:id},
+        { username, items, shippingAddress   },
+        { new: true }
+      );
+  
+      if (!updatedstore) {
+        return res.status(404).send({ message: "store not found" });
+      }
+  
+      res.status(200).send({ message: "store updated successfully", store: updatedstore });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ message: "Error updating store",error });
     }
   });
 
